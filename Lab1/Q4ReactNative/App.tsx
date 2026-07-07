bc7fedc7d6f42ea5c8dd8f8b965b15566722f4ea
Nguyen Dinh Thien <nguyendinhthien933@gmail.com>
2026-07-07 22:54:14 +0700

    # upload lab 1

---
+++ Lab1/Q4ReactNative/App.tsx
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NewAppScreen } from '@react-native/new-app-screen';
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import data from './Data';
+import Square from './Square';
+import styles from './style';
+import { ScrollView } from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppContent />
+    </SafeAreaProvider>
+  );
+}
+
+function AppContent() {
+  const safeAreaInsets = useSafeAreaInsets();
+
+  return (
+    <View style={styles.container}>
+      <NewAppScreen
+        templateFileName="App.tsx"
+        safeAreaInsets={safeAreaInsets}
+      />
+    </View>
+  );
+}
+
+
+
+export default App = () => {
+  return (
+    <ScrollView style={styles.container}>
+      {data.map((item, index) => (
+        <Square key={item} text={`Square ${index + 1}`} />
+      ))}
+    </ScrollView>
+  );
+};
---
+++ Lab1/Q4ReactNative/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export default style = StyleSheet.create({
+  container: { backgroundColor: '#fff' },
+  box: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center  ',
+    margin: 20,
+  },
+});
